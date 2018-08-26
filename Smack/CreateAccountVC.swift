71d427c73197d4753e86c850616ac17723018529
Ryan Morrison <egodev@icloud.com>
2018-08-26 18:15:31 +0100

    Create account functionality plus rounded buttons

--- Smack/CreateAccountVC.swift
+++ Smack/CreateAccountVC.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    //MARK: Outlets
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var userImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
- 
+    
+    
+    //MARK: Buttons
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let email = emailText.text , emailText.text != "" else { return }
+        guard let pass = passwordText.text , passwordText.text != "" else { return }
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("Registered User")
+            }
+        }
+    }
+    
+    @IBAction func packAvatarPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+        
+    }
+    
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
