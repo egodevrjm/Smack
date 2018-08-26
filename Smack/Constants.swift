eaef7b8d7ebb0b6948bdcff509dcb54d99a14ae6
Ryan Morrison <egodev@icloud.com>
2018-08-26 17:41:35 +0100

    Added Auth service

--- Smack/Constants.swift
+++ Smack/Constants.swift
@@ -8,7 +8,18 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//MARK: URLs
+let BASE_URL = "https://buzzupchat.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 //MARK: Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+//MARK: UserDefaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
